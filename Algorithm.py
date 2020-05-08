ebe84b1ce834c3d477e53188baa0f75fee0e1532
AJThompson6 <57770188+AJThompson6@users.noreply.github.com>
2020-05-07 22:24:49 -0500

    Add files via upload

--- Algorithm.py
+++ Algorithm.py
@@ -1,23 +1,18 @@
 # Author: Austin Thompson
 # File: Algorithm.py
 # Date: 4/23/2020
+    # 5/5/2020: Added comments
 
 
 from abc import ABC, abstractmethod
 import random
 import time
 
+
+#Algorithm class interface
 class AlgorithmInterface(ABC):
     @abstractmethod
     def __init__(self, name, size, min_value, max_value, data):
-        pass
-    
-    @abstractmethod
-    def display(self):
-        pass
-        
-    @abstractmethod
-    def pause(self):
         pass
     
     @abstractmethod
@@ -45,8 +40,9 @@ class AlgorithmInterface(ABC):
         pass
     
 
-
+# Algorithm class to handle non-GUI sorting activities
 class Algorithm(AlgorithmInterface):
+    # Initializer - Constructor
     def __init__(self, name, size, min_value, max_value, data):
         self.name = name
         self.size = size
@@ -54,99 +50,140 @@ class Algorithm(AlgorithmInterface):
         self.max_value = max_value
         self.data = data
         
-    def display(self):
-        print("display")
-        
-    def pause(self):
-        print("pause")
-        
+    # Generate random array method
     def generate_random_array(self):
-        #clear the attribute
+        
+        # Clear data every time method is called
         self.data = []
         x = 0
+        
+        # Keep appending items to the array for the objects given size
         while x < self.size:
+            
+            # Generate a random number between the min and max values of the object
             self.data.append(random.randrange(self.min_value, self.max_value + 1))
             x += 1
+            
+        # Return the randomly generated array
         return self.data
         
         
+    # Animation for bubble sort - swap adjacent elements if they are unsorted
     def animate_bubble(self, canvas, draw, speed):
+        # Iterate of the entire array
         for x in range (len(self.data)-1):
+            
             for j in range (len(self.data)-1):
+                
+                # If index j is greater than j+1 then swap
                 if self.data[j] > self.data[j+1]:
                     self.data[j], self.data[j+1] = self.data[j+1], self.data[j]
+                    
+                    # If x is equal to j or j+1 then change their color to signal they are being swapped
+                    # Call the draw method from GUI Driver to visualize the sorting algorithm and wait the object's set time
                     draw(self, canvas, ['green' if x == j or x == j+1 else 'orange' for x in range(len(self.data))] )
                     time.sleep(float(speed.get()))
         draw(self, canvas, ['green' for x in range(len(self.data))])
         
         
+    
+    # Animate insertion sort
     def animate_insertion(self, canvas, draw, speed):
         for index in range(1, len(self.data)):
+            
             # Find the next value to be sorted
             value = self.data[index]
             i = index - 1
             
+            # Draw the array and wait to help visualize
             draw(self, canvas, ['orange' if i<index else 'red' if self.data[i] == value else 'white' for i in range(len(self.data))] )
             time.sleep(float(speed.get()))
             
-            # Locate the position for the value to be inserted
+            # Locate the position for the value to be inserted and swap them
             while i >= 0 and value < self.data[i]:
                 self.data[i+1] = self.data[i]
                 i = i - 1
             
+            # Update the key value
             self.data[i+1] = value
+            
+            # Draw the updated array
             draw(self, canvas, ['orange' if i<index else 'red' if self.data[i] == value else 'white' for i in range(len(self.data))] )
             time.sleep(float(speed.get()))
             
-            
+        # Once sorting is completed, make all items the same color
         draw(self, canvas, ['orange' for i in range(len(self.data))] )
         time.sleep(float(speed.get()))
         
         
+        
+    
+    # Animate quick sort
     def animate_quick(self, canvas, draw, speed, head, tail):
         
         if head < tail:
             index_partition = self.partition(canvas, draw, speed, head, tail)
             
-            #left partition
+            # Left partition
             self.animate_quick(canvas, draw, speed, head, index_partition-1)
-            #right partition
+            
+            # Right partition
             self.animate_quick(canvas, draw, speed, index_partition+1, tail)
             
+            # Make all items white once completed sorted
             draw(self, canvas, ['white' for x in range(len(self.data))])
         
-        
+    
+    # Partition for quick sort
     def partition(self, canvas, draw, speed, head, tail):
-        border = head
+        
+        # Create edge of partition and pivot values for sorting
+        edge = head
         pivot = self.data[tail]
         
-        draw(self, canvas, self.color_array(len(self.data), head, tail, border, border))
+        # Draw the values and wait
+        draw(self, canvas, self.color_array(len(self.data), head, tail, edge, edge))
         time.sleep(float(speed.get()))
         
+        # Iterate over the array
         for j in range(head, tail):
+            
+            # If current index is less than the pivot swap
             if self.data[j] < pivot:
-                draw(self, canvas, self.color_array(len(self.data), head, tail, border, j, True))
+                
+                # Draw before swap
+                draw(self, canvas, self.color_array(len(self.data), head, tail, edge, j, True))
                 time.sleep(float(speed.get()))
                 
-                self.data[border], self.data[j] = self.data[j], self.data[border]
-                border += 1
-            draw(self, canvas, self.color_array(len(self.data), head, tail, border, tail, True))
+                # Swap and update the edge of the partition
+                self.data[edge], self.data[j] = self.data[j], self.data[edge]
+                edge += 1
+            
+            # Draw after the swap
+            draw(self, canvas, self.color_array(len(self.data), head, tail, edge, tail, True))
             time.sleep(float(speed.get()))
                 
-            
-        draw(self, canvas, self.color_array(len(self.data), head, tail, border, tail, True))
+        # Draw all in one color once complete
+        draw(self, canvas, self.color_array(len(self.data), head, tail, edge, tail, True))
         time.sleep(float(speed.get()))
         
-        self.data[border], self.data[tail] = self.data[tail], self.data[border]
-        return border
+        # Swap edge and tail values before returning the new partion
+        self.data[edge], self.data[tail] = self.data[tail], self.data[edge]
+        return edge
     
     
+    # Color array method for quick sort - this creates the returned array for the draw method
+        # Abstracted from partition method to simplify system functionality
     def color_array(self, length, head, tail, partition, index, swapping = False):
         
-        # create an empty color array to make displaying algorithm easier
+        # Create an empty color array to make displaying algorithm easier
         array = []
         
+        
+        # Check to see what each value in the array is and assign it a color based on
+            # the quick sort's current point in sorting
         for z in range(length):
+            # Set the borders to grey
             if z >= head and z <= tail:
                 array.append('grey')
             else:
@@ -156,8 +193,6 @@ class Algorithm(AlgorithmInterface):
                 array[z] = 'blue'
             elif z == partition:
                 array[z] = 'red'
-            elif z == index:
-                array[z] = 'yellow'
             
             if swapping:
                 if z == partition or z == index:
--- GUI_Driver.py
+++ GUI_Driver.py
@@ -12,6 +12,8 @@ from Tutorial import Tutorial
 import subprocess
 import random
 import tkinter
+import io
+from contextlib import redirect_stdout
 
 
 
@@ -55,7 +57,7 @@ class Login(tk.Frame):
         login_frame = tk.Frame(self, width=200, height=100)
         login_frame.pack()
 
-        label = tk.Label(self, text = "Enter your user information", font = LARGE_FONT)
+        label = tk.Label(self, text = "Enter your user information", font = MENU_FONT)
         label.pack(pady = 10, padx = 10)
 
         #username box        
@@ -112,7 +114,7 @@ class CreateAccount(tk.Frame):
         create_account_frame = tk.Frame(self, width=200, height=100)
         create_account_frame.pack()
 
-        label = tk.Label(self, text = "Create your username and password", font = LARGE_FONT)
+        label = tk.Label(self, text = "Create your username and password", font = MENU_FONT)
         label.pack(pady = 10, padx = 10)
 
         #username box        
@@ -156,11 +158,12 @@ class CreateAccount(tk.Frame):
             B1 = ttk.Button(popup, text="Okay", command = popup.destroy)
             B1.pack()       
                     
-        
+
+# Austin Thompson
 class StartScreen(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text = "Main Menu", font = LARGE_FONT)
+        label = tk.Label(self, text = "Main Menu", font = MENU_FONT)
         label.pack(pady = 10, padx = 10)
         
         startTutorial = tk.Button(self, text = "Start Tutorials", command = lambda: controller.show_frame(Tutorial))
@@ -169,11 +172,11 @@ class StartScreen(tk.Frame):
         algVis = tk.Button(self, text = "Visualize Algorithm", command = lambda: controller.show_frame(Algorithm_Visualizer))
         algVis.pack()
         
-    
+        
 class Tutorial(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text = "Select Tutorials", font = LARGE_FONT)
+        label = tk.Label(self, text = "Select Tutorials", font = MENU_FONT)
         label.pack(pady = 10, padx = 10)
         
         beginner = tk.Button(self, text = "Beginner", command=lambda:controller.show_frame(Beginner))
@@ -189,11 +192,11 @@ class Tutorial(tk.Frame):
         Home.place(relx=.5, rely=.95, anchor=CENTER)
 
         
-        
+# John Huyhn
 class Beginner(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text = "Beginner Tutorials", font = LARGE_FONT)
+        label = tk.Label(self, text = "Beginner Tutorials", font = MENU_FONT)
         label.pack(pady = 10, padx = 10)
 
         # Problem Description
@@ -205,7 +208,6 @@ class Beginner(tk.Frame):
         TextArea.place(rely=.30, relx=.18)
 
         # Submit Button / sends to textprint method
-
         Submit = tk.Button(self, text="Submit", command=lambda: checkInput(TextArea))
 
         Submit = tk.Button(self, text="Submit", command=lambda: textprint(TextArea))
@@ -217,9 +219,6 @@ class Beginner(tk.Frame):
         solution = tk.Button(self, text="Solution", command=lambda: controller.show_frame(bSolution()))
         solution.place(relx=.5, rely=.9, anchor=CENTER)
         solution.pack()
-
-        solution = tk.Button(self, text="Solution", command=lambda: controller.show_frame(Tutorial))
-        solution.place(relx=.5, rely=.9, anchor=CENTER)
         
         Back = tk.Button(self, text = "Back", command=lambda: controller.show_frame(Tutorial))
         Back.place(relx = .5, rely = .95, anchor = CENTER)
@@ -228,7 +227,7 @@ class Beginner(tk.Frame):
 class Intermediate(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text = "Intermediate Tutorials", font = LARGE_FONT)
+        label = tk.Label(self, text = "Intermediate Tutorials", font = MENU_FONT)
         label.pack(pady = 10, padx = 10)
 
         #Problem Description
@@ -248,13 +247,9 @@ class Intermediate(tk.Frame):
         Submit.pack()
 
         # Solution button
-
         solution = tk.Button(self, text="Solution", command=lambda: controller.show_frame(iSolution()))
         solution.place(relx=.5, rely=.9, anchor=CENTER)
         solution.pack()
-
-        solution = tk.Button(self, text="Solution", command=lambda: controller.show_frame(Tutorial))
-        solution.place(relx=.5, rely=.9, anchor=CENTER)
 
 
         Back = tk.Button(self, text="Back", command=lambda: controller.show_frame(Tutorial))
@@ -264,7 +259,7 @@ class Intermediate(tk.Frame):
 class Hard(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text = "Hard Tutorials", font = LARGE_FONT)
+        label = tk.Label(self, text = "Hard Tutorials", font = MENU_FONT)
         label.pack(pady = 10, padx = 10)
 
         # Problem Description
@@ -290,139 +285,21 @@ class Hard(tk.Frame):
         solution.place(relx=.5, rely=.9, anchor=CENTER)
         solution.pack()
 
-        solution = tk.Button(self, text="Solution", command=lambda: controller.show_frame(Tutorial))
-        solution.place(relx=.5, rely=.9, anchor=CENTER)
-
-
         Back = tk.Button(self, text="Back", command=lambda: controller.show_frame(Tutorial))
         Back.place(relx=.5, rely=.95, anchor=CENTER)
-
-
-class Algorithm_Visualizer(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text = "Select Algorithm", font = LARGE_FONT)
-        label.pack(pady = 10, padx = 10)
         
         
-        # Create a "UI" for the user to generate a new array, etc...
-        algorithm_frame = tk.Frame(self, width=200, height=100)
-        algorithm_frame.pack()
-        
-        
-        # Create a label to select the algorithm type
-        type_label = tk.Label(algorithm_frame, text = "Algorithm Type: ")
-        type_label.pack(side=LEFT)
-        # Dropdown menu to select type
-        type_algorithm = ttk.Combobox(algorithm_frame, values=['Bubble Sort', 'Insertion Sort', 'Quick Sort'])
-        type_algorithm.pack(side=LEFT)
-        type_algorithm.current(0)
-        
-        # Enter the size of the array
-        array_size = tk.Label(algorithm_frame, text = "Size (5-20): ")
-        array_size.pack(padx = 15)
-        entrysize = tk.Entry(algorithm_frame)
-        entrysize.focus()
-        entrysize.pack(pady=5)
-
-        min_value = 20
-        max_value = 200
-        data = []
-        chosen_algorithm = Algorithm(type_algorithm.get(), 10, min_value, max_value, data)
-        
-        # Slider to allow user to adjust playback speed
-        speed = tk.Label(algorithm_frame, text = "Playback Speed")
-        speed.pack()
-
-        alg_speed = Scale(algorithm_frame, from_= 0.0, to = 3.0, length = 200, digits = 2, resolution = 0.1, orient = HORIZONTAL)
-        alg_speed.place(relx=0.5, rely=.9, anchor=CENTER)
-        
-        canvas = Canvas(self, width=600, height=350, bg='snow2')
-        create_array = tk.Button(self, text="Create New Array", command=lambda:Generate(chosen_algorithm, entrysize, canvas), bg='orange')
-        create_array.place(relx=.43, rely=.20)
-        
-        start_visualizing = tk.Button(self, text="Animate", command = lambda:Animate(chosen_algorithm, canvas, alg_speed, type_algorithm), width = 13)
-        start_visualizing.place(relx=.43, rely=.24)
-        
-        Home = tk.Button(self, text = "Home", command=lambda: controller.show_frame(StartScreen))
-        Home.place(relx = .5, rely = .95, anchor = CENTER)
-        
-
-def Generate(algorithm, entrysize, canvas):
-    if len(entrysize.get()) == 0:
-        algorithm.size = 10
-    else:
-        algorithm.size = int(entrysize.get())
-    
-    # Simple logic to correct invalid inputs
-    if algorithm.size > 20:
-        algorithm.size = 20
-    elif algorithm.size <= 0:
-        algorithm.size = 10
-        
-    # Generate the random array
-    algorithm.data = algorithm.generate_random_array()
-    
-    # call draw which displays the items in the array
-    draw(algorithm, canvas, ['orange' for x in range(len(algorithm.data))])
-    
-def draw(algorithm, canvas, color_list):
-    
-    # Remove items in the canvas to draw on top of them
-    canvas.delete("all")
-    #create an output screen
-    bar_height = 350
-    width = 600
-    offset = 20
-    spacing = 10
-    bar_width = width / (len(algorithm.data) + 1)
-     
-    # Unnecessary but makes differences between values easier to see
-    normalized_data = [i / max(algorithm.data) for i in algorithm.data]
- 
-    # Iterate over list of normalized data
-    for i, height in enumerate(normalized_data):
-        
-        # create_rectangle needs positions of two opposite
-        # corners to draw a rectange so calculate based on canvas size
-        x = i * (bar_width) + offset + spacing
-        y = bar_height - height * 300
-        x1 = (i + 1) * (bar_width) + offset
-        y1 = bar_height
-        
-        # Draw the rectangles
-        canvas.create_rectangle(x, y, x1, y1, fill = color_list[i])
-        canvas.create_text(x, y, anchor=SW, text=str(algorithm.data[i]))
-        
-    # Update each time an item is moved so that the user can see when an item in the array is moved
-    root.update()
-    canvas.place(rely=.30, relx=.15)
-     
-def Animate(algorithm, canvas, alg_speed, type_algorithm):
-    algorithm.name = type_algorithm.get()
-    
-    if algorithm.name == "Insertion Sort":
-        algorithm.animate_insertion(canvas, draw, alg_speed)
-        
-    elif algorithm.name == "Bubble Sort":
-        algorithm.animate_bubble(canvas, draw, alg_speed)
-        
-    elif algorithm.name == "Quick Sort":
-        array = algorithm.data
-        algorithm.animate_quick(canvas, draw, alg_speed, 0, len(array)-1)
-
 def checkInput(TextArea):
     input = TextArea.get("1.0", END)
     Tutorial.check_input(input)
 
 
 def textprint(TextArea):
-    #file_ = open("output.txt", "w")
     if "import os" in TextArea.get("1.0",END):
         print("You can't import that!!! That's dangerous!")
     else:
-        output = subprocess.check_output(exec(TextArea.get("1.0",END)))
-        print(output)
+        exec(TextArea.get("1.0",END))
+        
 
 
 def bSolution():
@@ -436,11 +313,142 @@ def iSolution():
 def hSolution():
     s = open("hSolution.txt", "r")
     messagebox.showinfo("Solution", s.read())
+    
+    
+
+
+# Author - Austin Thompson
+# Take user to the algorithm visualizer menu
+class Algorithm_Visualizer(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text = "Select Algorithm", font = MENU_FONT)
+        label.pack(pady = 10, padx = 10)
+        
+        
+        # Create a "UI" for the user to generate a new array, etc...
+        algorithm_frame = tk.Frame(self, width=200, height=100)
+        algorithm_frame.pack()
+        
+        
+        # Create a label to select the algorithm type
+        type_label = tk.Label(algorithm_frame, text = "Algorithm Type: ")
+        type_label.pack(side=LEFT)
+        
+        # Dropdown menu to select type of algorithm user would like to see
+        type_algorithm = ttk.Combobox(algorithm_frame, values=['Bubble Sort', 'Insertion Sort', 'Quick Sort'])
+        type_algorithm.pack(side=LEFT)
+        type_algorithm.current(0)
+        
+        # Enter the size of the array
+        array_size = tk.Label(algorithm_frame, text = "Size (5-20): ")
+        array_size.pack(padx = 15)
+        entrysize = tk.Entry(algorithm_frame)
+        entrysize.focus()
+        entrysize.pack(pady=5)
+
+        # Preset the min and max values of the array
+        min_value = 20
+        max_value = 200
+        
+        # Create the algorithm object - intialize the data array to empty
+        chosen_algorithm = Algorithm(type_algorithm.get(), 10, min_value, max_value, data=[])
+        
+        # Slider to allow user to adjust playback speed
+        speed = tk.Label(algorithm_frame, text = "Playback Speed(s)")
+        speed.pack()
+
+        alg_speed = Scale(algorithm_frame, from_= 0.0, to = 3.0, length = 200, digits = 2, resolution = 0.1, orient = HORIZONTAL)
+        alg_speed.place(relx=0.5, rely=.9, anchor=CENTER)
+        
+        # Create a canvas to display the array that will be sorted
+        canvas = Canvas(self, width=600, height=350, bg='snow2')
+        create_array = tk.Button(self, text="Create New Array", command=lambda:Generate(chosen_algorithm, entrysize, canvas), bg='orange')
+        create_array.place(relx=.43, rely=.20)
+        
+        # Animate button - calls Animate method - then calls sorting algorthm in Algorithm.py to sort
+        visualize = tk.Button(self, text="Animate", command = lambda:Animate(chosen_algorithm, canvas, alg_speed, type_algorithm), width = 13)
+        visualize.place(relx=.43, rely=.24)
+        
+        Home = tk.Button(self, text = "Home", command=lambda: controller.show_frame(StartScreen))
+        Home.place(relx = .5, rely = .95, anchor = CENTER)
+        
+
+# Generate - random array
+def Generate(algorithm, entrysize, canvas):
+    if len(entrysize.get()) == 0:
+        algorithm.size = 10
+    else:
+        algorithm.size = int(entrysize.get())
+    
+    # Simple logic to correct invalid inputs
+        # Keeps size of array between 5 and 20
+    if algorithm.size > 20:
+        algorithm.size = 20
+    elif algorithm.size <= 4:
+        algorithm.size = 10
+        
+    # Generate the random array - call algorithms generate array method
+    algorithm.data = algorithm.generate_random_array()
+    
+    # call draw which displays the items in the array
+    draw(algorithm, canvas, ['orange' for x in range(len(algorithm.data))])
+    
+    
+# draw - takes the algorithm object, canvas, and a color array to display colors of individual items in list
+def draw(algorithm, canvas, color_list):
+    
+    # Remove items in the canvas to draw on top of them
+    canvas.delete("all")
+    
+    # Create space to draw items
+    bar_height = 350
+    width = 600
+    offset = 20
+    spacing = 10
+    
+    # Calculate width of each bar based on canvas size
+    bar_width = width / (len(algorithm.data) + 1)
+     
+    # Normalize the data array - Unnecessary but makes differences between values easier to see
+    normalized_data = [i / max(algorithm.data) for i in algorithm.data]
+ 
+    # Iterate over list of normalized data
+    for i, height in enumerate(normalized_data):
+        
+        # Create_rectangle needs positions of two
+            # Corners to draw a rectangle so calculate based on canvas size
+        x = i * (bar_width) + offset + spacing
+        y = bar_height - height * 300
+        x1 = (i + 1) * (bar_width) + offset
+        y1 = bar_height
+        
+        # Draw the rectangles
+        canvas.create_rectangle(x, y, x1, y1, fill = color_list[i])
+        canvas.create_text(x, y, anchor=SW, text=str(algorithm.data[i]))
+        
+    # Update each time an item is moved so that the user can see when an item in the array is moved
+    root.update()
+    canvas.place(rely=.30, relx=.15)
+     
+# Animate - calls algorithm objects sorting methods based on the type of algorithm selected
+def Animate(algorithm, canvas, alg_speed, type_algorithm):
+    algorithm.name = type_algorithm.get()
+    
+    if algorithm.name == "Insertion Sort":
+        algorithm.animate_insertion(canvas, draw, alg_speed)
+        
+    elif algorithm.name == "Bubble Sort":
+        algorithm.animate_bubble(canvas, draw, alg_speed)
+        
+    elif algorithm.name == "Quick Sort":
+        array = algorithm.data
+        algorithm.animate_quick(canvas, draw, alg_speed, 0, len(array)-1)
 
     
     
     
-LARGE_FONT = ("Times New Roman", 17)
+MENU_FONT = ("Times New Roman", 17)
 PROB_FONT = ("Times New Roman", 14)
 root = GUI_Driver()
 root.title("Introduction To Basic Programming Concepts")
